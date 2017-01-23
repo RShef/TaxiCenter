638f597a626cd118cce45ef609cb4af6637a4c31
othaller <othaller@gmail.com>
2017-01-23 18:44:44 +0200

    Map, Cab & task validation

--- MainFlow/ServerMain.cpp
+++ MainFlow/ServerMain.cpp
@@ -27,8 +27,10 @@ int findCar(char carInput) {
         return 1;
     } else if (carInput == 'T') {
         return 2;
-    } else {
+    } else if (carInput == 'F') {
         return 3;
+    } else {
+        return -1;
     }
 }
 
@@ -46,9 +48,45 @@ int findColor(char colorInput) {
         return 2;
     } else if (colorInput == 'P') {
         return 3;
-    } else {
+    } else if (colorInput == 'W') {
         return 4;
+    } else {
+        return -1;
     }
+}
+
+/**
+* Check validity of map input.
+* @return - 0 if input is valid, -1 otherwise.
+*/
+int validateMap(int x, int y, vector <GridPoint*> obstacles, char comma) {
+    if (x <= 0 || y <= 0) {
+        return -1;
+    }
+    for (int i = 0; i < obstacles.size(); ++i) {
+        int obsX = obstacles.at(i)->x;
+        int obsY = obstacles.at(i)->y;
+        if (obsX <= 0 || obsX >= x || obsY <= 0 || obsY >= y || comma != ',') {
+            return -1;
+        }
+    }
+    return 0;
+}
+
+/**
+* Check validity of cab input.
+* @return - 0 if input is valid, -1 otherwise.
+*/
+int validateCab(int id, int type, int car, int color, char comma[]) {
+    if (id < 0 || type < 1 || type > 2 || car == -1 || color == -1) {
+        return -1;
+    }
+    for (int i = 0; i < 7; ++i) {
+        if (comma[i] != ',') {
+            return -1;
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
@@ -61,28 +99,37 @@ int main(int argc, char *argv[]) {
     int gridX, gridY, numOfObstacles;
     int task;
     int obsX, obsY;
-    char comma[7];
+    char comma[] = {',',',',',',',',',',',',','};
     int input1, input2, input3, input4, input5, input6, input7;
     char input8, input9;
     double input10;
     vector <GridPoint*> obstacles;
 
     Server* server = new Server(atoi(argv[1]));
-    
-    // construct the grid (our WORLD).
-    cin >> gridX >> gridY >> numOfObstacles;
-    if (numOfObstacles > 0) {
-        for (int i = 0; i < numOfObstacles; ++i) {
-            // Get input from user.
-            cin >> obsX >> comma[0] >> obsY;
-            obstacles.push_back(new GridPoint(obsX, obsY));
+
+    while(true) {
+        // construct the grid (our WORLD).
+        cin >> gridX >> gridY >> numOfObstacles;
+        if (numOfObstacles > 0) {
+            for (int i = 0; i < numOfObstacles; ++i) {
+                // Get input from user.
+                cin >> obsX >> comma[0] >> obsY;
+                obstacles.push_back(new GridPoint(obsX, obsY));
+            }
+        }
+        if (validateMap(gridX, gridY, obstacles, comma[0]) == 0) {
+            server->setUp(gridX, gridY, obstacles);
+            break;
+        } else {
+            LOG(ERROR) << "Map input incorrect. please try again.";
+            cout << "-1" << endl;
+            obstacles.clear();
         }
     }
-    server->setUp(gridX, gridY, obstacles);
-    
+
     // run tasks loop
     cin >> task;
-    while (task != 0) {
+    while (true) {
 
         switch (task) {
 
@@ -103,10 +150,19 @@ int main(int argc, char *argv[]) {
 
             case 3: // insert new vehicle
             {
-                cin >> input1 >> comma[0] >> input2 >> comma[1] >> input8 >> comma[2] >> input9;
-                int car = findCar(input8);
-                int color = findColor(input9);
-                server->three(input1, input2, car, color);
+                while (true) {
+                    cin >> input1 >> comma[0] >> input2 >> comma[1] >> input8 >> comma[2] >> input9;
+                    int car = findCar(input8);
+                    int color = findColor(input9);
+                    if (validateCab(input1, input2, car, color, comma) == 0) {
+                        server->three(input1, input2, car, color);
+                        break;
+                    } else {
+                        LOG(ERROR) << "Invalid cab input. Please try again";
+                        cout << "-1" << endl;
+                        memset(comma, ',', sizeof(comma));
+                    }
+                }
                 break;
             }
 
@@ -132,6 +188,8 @@ int main(int argc, char *argv[]) {
 
 
             default: {
+                LOG(ERROR) << "Invalid task number. please try again.";
+                cout << "-1" << endl;
                 break;
             }
         }
